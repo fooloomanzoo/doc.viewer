3adb031b48428c2737446fd754e842b92be9ddea
fooloomanzii <hannes-b@web.de>
2016-10-28 18:42:52 +0200

    tests

--- test/tools/convertAllWithPandoc.bat
+++ test/tools/convertAllWithPandoc.bat
@@ -1,7 +1,4 @@
-# @echo off
-
-mkdir output
-for /r %%i in (*.*) DO call pandoc %%i -s -t epub -o output/%%i.epub
+for /f %%A in ('forfiles /s /m *.* /c "cmd /c echo @relpath"') do cmd /c ""C:\Program Files (x86)\Pandoc\pandoc.exe" %%~A -s -t epub -o output\%%~A.epub"
 
 pause
 /WAIT
